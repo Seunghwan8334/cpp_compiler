24709b6856a28da079fb8289a1c1b0bbbe0b94c9
최민혁 <choiminhyeok8334@gmail.com>
2025-01-02 01:59:54 +0900

    일단 기본적인 건 완성

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 @echo off
-set filename_with_ext=%1
+set filename_with_ext="%1"
 set filename=%filename_with_ext:.cpp=%
 set file_cpp=%filename%.cpp
 set file_exe=%filename%.exe 
@@ -7,9 +7,8 @@ set file_exe=%filename%.exe
 if exist %file_exe% del %file_exe%
 g++ %file_cpp% -o %file_exe%
 
-if exist %file_exe%(
-    %file_exe%
+if exist %file_exe% (
+    %file_exe% < input.txt > output.txt
 ) else (
     echo compile error.
 )
-
